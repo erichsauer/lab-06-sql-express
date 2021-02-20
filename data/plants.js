d3529367a422732ebe9e7eaa7ad10fcd2f69af55
Erich Sauer <erich.sauer@gmail.com>
2021-02-19 16:43:23 -0800

    it worrrrrkssssssss

---
+++ data/plants.js
@@ -0,0 +1,62 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Crested Fluffel',
+    image: 'crested-fluffel.jpg',
+    description: 'Very rare fruiting body with feather-like pollination whisps.',
+    fragrant: true,
+    category: 'annual',
+    price: 10,
+    owner: 1
+  },
+  {
+    id: 2,
+    name: 'Cragged Fallpop',
+    image: 'cragged-fallpop.jpg',
+    description: 'Generally appear in woodland settings among forest duff.',
+    fragrant: false,
+    category: 'annual',
+    price: 15,
+    owner: 1
+  },
+  {
+    id: 3,
+    name: 'Fluted Yodel',
+    image: 'fluted-yodel.jpg',
+    description: 'The Crested Fluffel ',
+    fragrant: true,
+    category: 'ephimeral',
+    price: 10,
+    owner: 1
+  },
+  {
+    id: 4,
+    name: 'Spring Zipsies',
+    image: 'spring-zipsies.jpg',
+    description: 'Small colonies often appear in lawns near Garden Gnomes.',
+    fragrant: true,
+    category: 'ephimeral',
+    price: 20,
+    owner: 1
+  },
+  {
+    id: 5,
+    name: 'Topsy Swayer',
+    image: 'topsy-swayer.jpg',
+    description: 'Almost impossible to find in nature; they thrive in a controlled environment.',
+    fragrant: false,
+    category: 'perennial',
+    price: 10,
+    owner: 1
+  },
+  {
+    id: 6,
+    name: 'Yuzie (Rare Form)',
+    image: 'yuzie-pollination-form.jpg',
+    description: 'Note the squiggle tips used to attract a pollination partner.',
+    fragrant: false,
+    category: 'annual',
+    price: 10,
+    owner: 1
+  },
+];
